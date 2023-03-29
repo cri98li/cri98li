c1e36a0575deaec1d6bed9c6b06cde5eed1ecaf6
cri98li <c.landi7@studenti.unipi.it>
2023-03-29 15:12:22 +0200

    Links updates

--- docs/main.js
+++ docs/main.js
@@ -6,7 +6,7 @@ $.getJSON( "https://bing.biturl.top/?mkt=random&index=random", function( data ) 
     console.log(data)
     $.each( data, function( key, val ) {
         if(key === "url"){
-            document.body.style.background = "url('"+ val +"')";
+            document.body.style.backgroundImage = "url('"+ val +"')";
         }
     });
 });
